ba88b7adc7cec370d31a4e0d978cf66ee496f171
BVerstaen <bverstaen04@gmail.com>
2025-09-05 20:04:40 +0200

    push

--- Godfather2025Team32/Assets/Scripts/Victory/VictoryZone.cs
+++ Godfather2025Team32/Assets/Scripts/Victory/VictoryZone.cs
@@ -6,16 +6,20 @@ public class VictoryZone : MonoBehaviour
 
     private int _playerFinish = 0;
 
+    private PlayerController firstPlayer;
     private void OnTriggerEnter(Collider other)
     {
         print(other.gameObject.name);
         PlayerController player = other.GetComponent<PlayerController>();
         if (player != null)
         {
+            if(firstPlayer == null)
+                firstPlayer = player;
+
             _playerFinish++;
 
             if (_playerFinish == _numberOfPlayers)
-                GameManager.Instance.SetRoundWinner(player.currentTeam);
+                GameManager.Instance.SetRoundWinner(firstPlayer.currentTeam);
         }
     }
 }
